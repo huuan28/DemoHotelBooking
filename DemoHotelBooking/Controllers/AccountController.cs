f9853bcd250ffe0b2b36d7dcaafb0b0ef501687a
No Hope <haov03200@gmail.com>
2024-06-25 15:05:23 +0700

    frontend room and booking

--- DemoHotelBooking/Controllers/AccountController.cs
+++ DemoHotelBooking/Controllers/AccountController.cs
@@ -59,10 +59,6 @@ namespace DemoHotelBooking.Controllers
             }
             return View(model);
         }
-        public IActionResult Login()
-        {
-            return View();
-        }
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
